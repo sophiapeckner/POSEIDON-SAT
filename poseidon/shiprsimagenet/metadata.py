e665d729361345f4d14f6bcc65f4a2276b3199de
Kyler Nelson <kyler.nelson@usu.edu>
2024-02-14 21:52:29 -0700

    Add code to parse instances of objects from annotations in a given imageset

---
+++ poseidon/shiprsimagenet/metadata.py
@@ -0,0 +1,61 @@
+import os
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+
+class HorizontalBoundingBox:
+    def __init__(self, xmin: int, ymin: int, xmax: int, ymax: int):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+
+
+class OrientedBoundingBox:
+    def __init__(self, cx: int, cy: int, width: int, height: int, rot: float):
+        self.center_x = cx
+        self.center_y = cy
+        self.width = width
+        self.height = height
+        self.rotation = rot
+    
+
+class LabeledObject:
+    def __init__(self, filename: str, name: str, truncated: bool, difficult: bool, bndbox: HorizontalBoundingBox, rotated_box: OrientedBoundingBox, levels: 'tuple[int, int, int, int]'):
+        self.filename = filename
+        self.name = name
+        self.truncated = truncated
+        self.difficult = difficult
+        self.bndbox = bndbox
+        self.rotated_box = rotated_box
+        self.level_group = levels
+
+def parse_voc(root_path: str, image_set: str):
+    labeled_objects: list[LabeledObject] = []
+    image_set_path = os.path.join(root_path, 'ImageSets', 'Main', f'{image_set}.txt')
+    with open(image_set_path, 'r') as f:
+        image_names = f.read().splitlines()
+
+    for image_name in image_names:
+        annotation_path = os.path.join(root_path, 'Annotations', f'{Path(image_name).stem}.xml')
+        tree = ET.parse(annotation_path)
+        root = tree.getroot()
+        filename = root.find('filename').text
+
+        for obj in root.findall('object'):
+            name = obj.find('name').text
+            truncated = obj.find('truncated').text == '1'
+            difficult = obj.find('difficult').text == '1'
+
+            bndbox = obj.find('bndbox')
+            bndbox = {child.tag: int(child.text) for child in bndbox}
+            
+            rotated_box = obj.find('rotated_box')
+            rotated_box = {child.tag: float(child.text) if child.tag == 'rot' else int(child.text) for child in rotated_box}
+
+            levels = tuple([int(obj.find(f'level_{i}').text) for i in range(4)])
+
+            labeled_object = LabeledObject(filename, name, truncated, difficult, HorizontalBoundingBox(**bndbox), OrientedBoundingBox(**rotated_box), levels)
+            labeled_objects.append(labeled_object)
+
+    return labeled_objects
