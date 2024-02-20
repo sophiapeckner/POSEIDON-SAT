6fe5717e9b3210477f2ddb1fe2393ceea2a905e4
Kyler Nelson <kyler.nelson@usu.edu>
2024-02-19 20:57:35 -0700

    Main script tweaks

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ if __name__ == '__main__':
     with open('selected_for_augmentation.txt', 'r') as f:
         images_to_process = f.read().splitlines()
 
-    print(f'Augmenting {len(images_to_process)} images...')
-
     # The original normalization method described by POSEIDON is not a good fit for the ShipRSImageNet dataset
     # We'll instead normalize images based on the source and destination spatial resolutions
     #print('Normalizing images...')
     #normalize(original_dataset.root_path, "ShipRSImageNet_V1_Normalized", images_to_process, 'train_level_2')
 
+    print('Compiling source images for generator...')
     extractor = InstanceExtractor(original_dataset)
     extractor.extract_instances(CLASS_TO_AUGMENT, 'fishing_vessel_instances')
+    print()
 
+    #print(f'Augmenting {len(images_to_process)} images...')
     #generator = COCOInstanceGenerator()
     #generator.balance('/Users/pabloruizponce/Vainas/POSEIDON/poseidon/outputs')
+    #print()
 
     #conversor = COCO2YOLO()
     #conversor.convert("/Users/pabloruizponce/Vainas/SDSYOLO", "SDSYOLO")
