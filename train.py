a75076aa4bfb65c8239688c0d2f9b326d8ca6fad
Kyler Nelson <kyler.nelson@usu.edu>
2024-02-27 19:55:42 -0700

    Setup project folder hierarchy in train script.

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import argparse
-
+from pathlib import Path
 from ultralytics import YOLO
 
 from yolo.dataset_util import check_dataset
@@ -13,7 +13,7 @@ def main():
     parser.add_argument('dataset', type=str, help='The name of the dataset directory containing the data to train on')
     parser.add_argument('-m', '--model', type=str, default='yolov8n', help='The YOLO model to train. Can be a custom model name or one of the built-in models. Default is yolov8n.')
     parser.add_argument('-e', '--epochs', type=int, default=100, help='The number of epochs to train for. Defaults to 300')
-    parser.add_argument('-n', '--run-name', type=str, default=None, help='The name of the run to use for outputs')
+    parser.add_argument('-n', '--run-name', type=str, default='default-project', help='The name of the run to use for outputs')
     parser.add_argument('-p', '--project', type=str, default=None, help='The name of the project directory to use for outputs')
     parser.add_argument('-r', '--resume', action='store_true', help='Resume training session from a previous run using the weights file specified with --model')
     parser.add_argument('-b', '--batch-size', type=int, default=16, help='The batch size to use for training. Set to -1 to use AutoBatch. Defaults to 16')
@@ -33,7 +33,7 @@ def main():
                epochs=args.epochs,
                batch=args.batch_size,
                name=args.run_name,
-               project=args.project,
+               project=str(Path('runs') / args.project),
                resume=args.resume,
                plots=not args.no_training_plots)
     
