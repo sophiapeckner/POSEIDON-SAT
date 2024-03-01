dea0c02f45fa91371a20b2cc781dbe934edce2ef
Kyler Nelson <kyler.nelson@usu.edu>
2024-02-29 22:09:19 -0700

    Fix validation loss stuck at infinity when weighting classes.

--- yolo/class_weighted_yolo.py
+++ yolo/class_weighted_yolo.py
@@ -14,3 +14,21 @@ class ClassWeightedDetectionModel(DetectionModel):
         # Modify classification loss for the model to use a BCE loss with class weights instead of an unwieghted BCE loss
         loss.bce = BCEWithLogitsLoss(reduction='none', pos_weight=self.class_weights)
         return loss
+
+
+    def loss(self, batch, preds=None):
+        # Need to convert predictions to float32 during loss computation so that loss computation is always done as float32
+        # to avoid floating point overflow When using a larger batch size.
+
+        if isinstance(preds, tuple):
+            preds = list(preds)
+            if isinstance(preds[1], list):
+                preds[1] = [p.float() for p in preds[1]]
+            else:
+                preds[1] = preds[1].float()
+            preds = tuple(preds)
+        elif preds is not None:
+            preds = preds.float()
+
+        results = super().loss(batch, preds)
+        return results
