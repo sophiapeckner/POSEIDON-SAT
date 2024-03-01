b79284cf79f0372d72a43a82fe3078d1a5f4ac24
Kyler Nelson <kyler.nelson@usu.edu>
2024-02-29 23:13:55 -0700

    Fix missing parameter for weighted class baseline training

--- train-baseline-jobs.sh
+++ train-baseline-jobs.sh
@@ -1,2 +1,2 @@
 python train.py -p baselines ShipRSImageNet_V1
-python train.py -p baselines -n train-class-weighting ShipRSImageNet_V1
+python train.py -p baselines -n train-class-weighting -c ShipRSImageNet_V1
