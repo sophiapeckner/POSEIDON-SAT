36cbba592db581d24f261d02060f32ecf1648462
Kyler Nelson <kyler.nelson@usu.edu>
2024-02-29 18:58:09 -0700

    Add scripts for running all different training job configurations.

---
+++ train-augmented-dataset-jobs.sh
@@ -0,0 +1,8 @@
+python train.py -n mixed-resolutions -p augmented ShipRSImageNet_V1_Augmented
+python train.py -n mixed-resolutions -p class-weighting-augmented -c ShipRSImageNet_V1_Augmented
+
+python train.py -n matched-resolutions -p augmented ShipRSImageNet_V1_Augmented_MatchedRes
+python train.py -n matched-resolutions -p class-weighting-augmented -c ShipRSImageNet_V1_Augmented_MatchedRes
+
+python train.py -n matched-resolutions-moreinstances -p augmented ShipRSImageNet_V1_Augmented_MatchedRes_MoreInstances
+python train.py -n matched-resolutions-moreinstances -p class-weighting-augmented -c ShipRSImageNet_V1_Augmented_MatchedRes_MoreInstances
---
+++ train-baseline-jobs.sh
@@ -0,0 +1,2 @@
+python train.py -p baselines ShipRSImageNet_V1
+python train.py -p baselines -n train-class-weighting ShipRSImageNet_V1
