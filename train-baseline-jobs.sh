a5653303dab9b1302fc4d1c65ce31842a6cdab26
Kyler Nelson <kyler.nelson@usu.edu>
2024-04-08 17:33:41 -0600

    Add run names explicitly for 300-epoch versions

--- train-baseline-jobs.sh
+++ train-baseline-jobs.sh
@@ -1,4 +1,4 @@
 python train.py -p baselines ShipRSImageNet_V1
-python train.py -e 300 -p baselines ShipRSImageNet_V1
+python train.py -e 300 -p baselines -n train2 ShipRSImageNet_V1
 python train.py -p baselines -n train-class-weighting -c ShipRSImageNet_V1
-python train.py -e 300 -p baselines -n train-class-weighting -c ShipRSImageNet_V1
+python train.py -e 300 -p baselines -n train-class-weighting2 -c ShipRSImageNet_V1
